74142cdc59c220be45069e29e7c1106e30d3dcd8
Jenni Peltola <jenni.peltola@hotmail.com>
2026-01-22 13:22:38 +0200

    Added assignment folders, started first exercises

---
+++ 01JavaScript Foundations/assignment01.js
@@ -0,0 +1,23 @@
+// Fix this code to use let and const correctly instead of var:
+
+var name = "React";
+
+name = "Next.js";
+
+var age = 21;
+
+age = 22;
+
+var birthYear = 2000;
+
+// Solution:
+
+let name = "React";
+
+name = "Next.js";
+
+let age = 21;
+
+age = 22;
+
+const birthYear = 2000;
